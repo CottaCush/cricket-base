dde33d22290c7fb7937c5655be4ba4d240f1127b
Taiwo Ladipo <ladipotaiwo01@gmail.com>
2018-09-27 13:01:12 +0100

    add production css for FontAwesomeAsset

--- src/assets/FontAwesomeAsset.php
+++ src/assets/FontAwesomeAsset.php
@@ -12,4 +12,8 @@ class FontAwesomeAsset extends BaseCricketAsset
     public $css = [
         self::ASSETS_PLUGINS_PATH . '/font-awesome/css/font-awesome.min.css'
     ];
+
+    public $productionCss = [
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+    ];
 }
